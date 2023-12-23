e7c05786afc671b9210db95623dbfaee4f3fe95a
chan <sniwoo98@naver.com>
2023-12-23 10:42:55 +0900

    fix : 출력타입이 str이라서 틀렸음

--- baekjoon/Race Results.py
+++ baekjoon/Race Results.py
@@ -1,11 +1,11 @@
 T = int(input())
 lst = []
 for _ in range(T):
-    i = input().split()
+    i = list(map(int, input().split()))
     lst.append(i)
 
-lst.sort(key= lambda x : x[0])
-lst.sort(key= lambda x : x[1])
 lst.sort(key= lambda x : x[2])
+lst.sort(key= lambda x : x[1])
+lst.sort(key= lambda x : x[0])
 for i in lst:
     print(f"{i[0]} {i[1]} {i[2]}")
